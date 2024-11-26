5559796034e37c0c7ae3680569fa4bbcc0843ded
Daniil Khrapovitskii <danya240926@gmail.com>
2024-11-26 18:17:20 +0300

    XCStrings support

---
+++ Sources/RswiftParsers/Resources/XCString.swift
@@ -0,0 +1,44 @@
+//
+//  XCString.swift
+//  Rswift
+//
+//  Created by Даниил Храповицкий on 26.11.2024.
+//
+
+import Foundation
+
+struct XCString: Decodable {
+	let sourceLanguage: String
+	let strings: [String: XCStringString]
+	let version: String
+}
+
+struct XCStringString: Decodable {
+	let localizations: [String: XCLocalization]?
+}
+
+struct XCLocalization: Decodable {
+	let stringUnit: XCStringUnit?
+	let variations: XCVariations?
+	let substitutions: [String: XCSubstitution]?
+}
+
+struct XCVariations: Decodable {
+	let plural: [String: XCPluralVariationsValue]?
+	let device: [String: XCPluralVariationsValue]?
+}
+
+struct XCPluralVariationsValue: Decodable {
+	let stringUnit: XCStringUnit?
+	let variations: XCVariations?
+}
+
+struct XCStringUnit: Decodable {
+	let value: String
+}
+
+struct XCSubstitution: Decodable {
+	let argNum: Int?
+	let formatSpecifier: String
+	let variations: XCVariations
+}
